07a421553ca2d5c38e51acd5177980e91f9aaada
komal <komalsinha1811@gmail.com>
2024-06-01 17:23:36 +0530

    added loading effect using nextjs

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,31 @@ const API_KEY = process.env.API_KEY
 import Results from '@/components/Results';
 
 
+
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || 'fetchTrending'
   const res = await fetch(
     `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    {
+      next: { revalidate: 1000 }
+    }
   );
+
+  // **** for seeing loading effect ****
+  // const res = await new Promise((resolve) => {
+  //   setTimeout(async () => {
+  //     const response = await fetch(
+  //       `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+  //       }?api_key=${API_KEY}&language=en-US&page=1`,
+  //       {
+  //         next: { revalidate: 10 }
+  //       }
+  //     );
+  //     resolve(response)
+  //   }, 2000)
+  // })
+
   const data = await res.json();
   if (!res.ok) {
     throw new Error('Failed to fetch data');
