1fb737914010f5a49e174f62b453cbdeba1d9d3a
komal <komalsinha1811@gmail.com>
2024-06-01 15:24:31 +0530

    created the navbar component

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -1,19 +1,3 @@
-// "use client";
-
-// import React from "react";
-// import { ThemeProvider as NextThemeProvider } from "next-themes";
-
-// function Providers({ children }) {
-//   return;
-//   <NextThemeProvider defaultTheme="system" attribute="class">
-//     <div className="text-gray-700 dark:text-gray-200 dark:bg-gray-700 min-h-screen select-none transition-colors duration-300">
-//       {children}
-//     </div>
-//   </NextThemeProvider>;
-// }
-
-// export default Providers;
-
 "use client";
 
 import { ThemeProvider } from "next-themes";
@@ -30,6 +14,4 @@ export default function Providers({ children }) {
     </ThemeProvider>
   );
 }
-
-// className =
-//   "text-gray-700 dark:text-gray-200 dark:bg-gray-700 min-h-screen select-none transition-colors duration-300";
+0;
